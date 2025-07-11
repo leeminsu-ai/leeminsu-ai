fd246c2debfaf86e3560bd767759cc3026c95ea5
leeminsu-ai <dlalstn85451569@gmail.com>
2025-07-11 11:47:58 +0900

    day2 내용

---
+++ day02/src/info/information.java
@@ -0,0 +1,9 @@
+package info;
+
+public class information {
+	public static void main(String[] args) {
+		System.out.println("React&Spring 활용 자바개발자 양성과정");
+		System.out.println("황인빈 강사님");
+		System.out.println("이민수");
+	}
+}
