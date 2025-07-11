3909d65f24794227e65549961c1ab28dabf2f476
leeminsu-ai <dlalstn85451569@gmail.com>
2025-07-11 14:36:41 +0900

    변수사용법

---
+++ day02/src/data/Test01데이터란.java
@@ -0,0 +1,24 @@
+package data;
+
+public class Test01데이터란 {
+
+	public static void main(String[] args) {
+		System.out.println("7-11");//글자
+		System.out.println(7-11);//수식	
+		//7500원 자장면 7그릇가격은?
+		System.out.println(7500*7);
+		//자장면 7500원 , 짬뽕 8000원 가격일떄 자장면 2그릇과 짬뽕 3그릇 가격은?
+		System.out.println(7500*2+8000*3);
+		//변수라는것을 쓴다면 ....
+		System.out.println();
+		int a=7500;//a라는 이름으로 7500 보관해둬라
+		int b=2;	//b라는 이름으로 2를 보관해둬라	
+		int c = 8000; //c라는 이름으로 8000을 보관해둬라!
+		int d =3; // ```
+		System.out.println(a*b);
+		System.out.println(c*d);
+		System.out.println(a*b+c*d);
+		
+	}
+
+}
---
+++ day02/src/data/Test02변수.java
@@ -0,0 +1,39 @@
+package data;
+
+public class Test02변수 {
+
+	public static void main(String[] args) {
+		//본격적인 변수 사용
+		//형태와 이름 값이 있어야함
+		byte a=100;
+		//byte b=200; //범위초과
+		System.out.println(a);
+		
+		int num=3000;
+		System.out.println(num);
+		num=9999;
+		System.out.println(num);
+		
+		long over=2200000000L; // long은 꼭표시를해야함
+		System.out.println(over);
+		
+		long oVer=300000000L; //자바는 대소문자를 구분함.
+		
+		long n=1234567; 
+		long m=n+n;
+		long o=n*n;
+		System.out.println(m);
+		System.out.println(o);
+		
+		//(중요)정수의 계산결과는 언제나 정수이다.
+		//특히 나눗셈을 조심.
+		
+		int apple=10;
+		int people=3;
+		System.out.println(apple/people); // 한 사람이 받을 사과개수
+		System.out.println(apple%people); // 남는 사과개수
+		
+		
+	}
+
+}
