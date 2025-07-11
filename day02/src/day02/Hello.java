6347c8eaaf533eb4b00c221f7d9e0740651ae1d9
leeminsu-ai <dlalstn85451569@gmail.com>
2025-07-11 11:37:42 +0900

    ㅇ

--- day02/src/day02/Hello.java
+++ day02/src/day02/Hello.java
@@ -1,7 +1,7 @@
-package day02;
+package day02; //위치정보 표시(필수)	
 
-public class Hello {
-	public static void main(String[] args)
+public class Hello {//클래스(집)
+	public static void main(String[] args)//메소드(방),메인메소드(거실)
 	{
 		System.out.println("Hello world!");
 		System.out.println("Hello Java!");
