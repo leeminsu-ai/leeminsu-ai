1b640652333561c55ddda6f0f13a98abd8446ee6
leeminsu-ai <dlalstn85451569@gmail.com>
2025-08-12 12:09:27 +0900

    ㅇ

---
+++ spring02/src/main/java/com/kh/spring02/HomeController.java
@@ -0,0 +1,12 @@
+package com.kh.spring02;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@RequestMapping("/")
+	public String home() {
+		return "Welcome~";
+	}
+}
